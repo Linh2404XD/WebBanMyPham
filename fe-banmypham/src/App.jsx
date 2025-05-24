f10dd3817ef9206a4444e7fe5cd3681e8b3d9933
Nguyen-Thi-Thanh-Ngan <21130116@st.hcmuaf.edu.vn>
2025-05-24 08:58:36 +0700

    feat: update page contact

--- fe-banmypham/src/App.jsx
+++ fe-banmypham/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import Login from './pages/login/login.jsx';
 import Checkout from './pages/checkout/checkout.jsx'
 // import ShopGrid from "./pages/shop-grid/shop-grid.jsx";
+import Contact from './pages/contact/contact.jsx'
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/checkout" element={<Checkout />} />
                 {/*<Route path="/shop-grid" element={<ShopGrid />} />*/}
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
     );
--- fe-banmypham/src/pages/contact/contact.jsx
+++ fe-banmypham/src/pages/contact/contact.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import './ContactForm.css';
+import styles from './Contact.module.css';
 import Header from '../../components/header.jsx';
 import Footer from '../../components/footer.jsx';
-
 
 const ContactForm = () => {
     return (
         <>
-            <Header/>
-            <div className="contact-container">
-                <div className="contact-info">
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.info}>
                     <h2>Contact Information</h2>
                     <p>Fill up the form and our team will get back to you within 24 hours</p>
                     <p>📞 +233543201893</p>
                     <p>📧 2122221110@gmail.com</p>
                 </div>
 
-                <div className="contact-form">
+                <div className={styles.form}>
                     <h1>Contact US</h1>
                     <p>Any Question or remarks? Just write us a message</p>
 
                     <form>
                         <label>Name</label>
-                        <input type="text" placeholder="Allen Jones"/>
+                        <input type="text" placeholder="Allen Jones" />
 
                         <label>Email</label>
-                        <input type="email" placeholder="aljay126@gmail.com"/>
+                        <input type="email" placeholder="aljay126@gmail.com" />
 
                         <label>Phone</label>
-                        <input type="tel" placeholder="+233546227893"/>
+                        <input type="tel" placeholder="+233546227893" />
 
                         <label>Message</label>
                         <textarea placeholder="Write your message" rows="4"></textarea>
@@ -37,7 +36,7 @@ const ContactForm = () => {
                     </form>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 };
