15e17214ac8cb6c3b6c6dafb27496fdf95a993dd
Linh2404XD <21130092@st.hcmuaf.edu.vn>
2025-05-30 12:31:45 +0700

    update components

--- fe-banmypham/src/components/productSlider.jsx
+++ fe-banmypham/src/components/productSlider.jsx
@@ -3,7 +3,25 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const ProductSlider = ({ title, slides }) => {
+
+const ProductItem = ({ href, imgSrc, name, price }) => {
+    const handleMouseDown = (e) => e.preventDefault();
+
+    return (
+        <a href={href} className="latest-product__item" onMouseDown={handleMouseDown}>
+            <div className="latest-product__item__pic">
+                <img src={imgSrc} alt={name} />
+            </div>
+            <div className="latest-product__item__text">
+                <h6>{name}</h6>
+                <span>{price}</span>
+            </div>
+        </a>
+    );
+};
+
+
+const ProductSlider = ({ title, productGroups }) => {
     const settings = {
         dots: false,
         infinite: true,
@@ -16,9 +34,11 @@ const ProductSlider = ({ title, slides }) => {
         <div className="latest-product__text">
             <h4>{title}</h4>
             <Slider {...settings}>
-                {slides.map((slideContent, index) => (
+                {productGroups.map((group, index) => (
                     <div className="latest-prdouct__slider__item" key={index}>
-                        {slideContent}
+                        {group.map((product, i) => (
+                            <ProductItem key={i} {...product} />
+                        ))}
                     </div>
                 ))}
             </Slider>
@@ -26,125 +46,98 @@ const ProductSlider = ({ title, slides }) => {
     );
 };
 
-// Dữ liệu slide dạng JSX như bạn muốn (có thể thêm sửa thoải mái)
+// Dữ liệu sản phẩm
 const latestProductSlides = [
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-1.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Crab Pool Security</h6>
-                <span>$30.00</span>
-            </div>
-        </a>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-2.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Crab Pool Security</h6>
-                <span>$30.00</span>
-            </div>
-        </a>
-    </>,
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-3.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Crab Pool Security</h6>
-                <span>$30.00</span>
-            </div>
-        </a>
-    </>,
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-1.jpg",
+            name: "Serum Chống Nắng Anessa Dưỡng Da Nâng Tông 30ml",
+            price: "529.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-2.jpg",
+            name: "Gel Chống Nắng Anessa Dưỡng Sáng Nâng Tông 90g",
+            price: "310.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-3.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/product/product-4.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
 ];
 
 const topRatedSlides = [
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-1.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Top Rated Product 1</h6>
-                <span>$40.00</span>
-            </div>
-        </a>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-2.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Top Rated Product 2</h6>
-                <span>$50.00</span>
-            </div>
-        </a>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-3.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Top Rated Product 3</h6>
-                <span>$60.00</span>
-            </div>
-        </a>
-    </>,
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-1.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Top Rated Product 4</h6>
-                <span>$70.00</span>
-            </div>
-        </a>
-    </>,
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-1.jpg",
+            name: "Serum Chống Nắng Anessa Dưỡng Da Nâng Tông 30ml",
+            price: "529.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-2.jpg",
+            name: "Gel Chống Nắng Anessa Dưỡng Sáng Nâng Tông 90g",
+            price: "310.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-3.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/product/product-4.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
 ];
 
 const reviewSlides = [
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-1.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Review Product 1</h6>
-                <span>$20.00</span>
-            </div>
-        </a>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-2.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Review Product 2</h6>
-                <span>$25.00</span>
-            </div>
-        </a>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-3.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Review Product 3</h6>
-                <span>$15.00</span>
-            </div>
-        </a>
-    </>,
-    <>
-        <a href="#" className="latest-product__item">
-            <div className="latest-product__item__pic">
-                <img src="/assets/img/latest-product/lp-1.jpg" alt="" />
-            </div>
-            <div className="latest-product__item__text">
-                <h6>Review Product 4</h6>
-                <span>$10.00</span>
-            </div>
-        </a>
-    </>,
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-1.jpg",
+            name: "Serum Chống Nắng Anessa Dưỡng Da Nâng Tông 30ml",
+            price: "529.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-2.jpg",
+            name: "Gel Chống Nắng Anessa Dưỡng Sáng Nâng Tông 90g",
+            price: "310.000₫",
+        },
+        {
+            href: "#",
+            imgSrc: "/assets/img/latest-product/lp-3.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
+    [
+        {
+            href: "#",
+            imgSrc: "/assets/img/product/product-4.jpg",
+            name: "Sữa Chống Nắng Anessa Dưỡng Da Kiềm Dầu 60ml (Bản Mới)",
+            price: "423.000₫",
+        },
+    ],
 ];
 
 const ProductsSection = () => {
@@ -152,19 +145,14 @@ const ProductsSection = () => {
         <section className="latest-product spad">
             <div className="container">
                 <div className="row">
-                    {/* Latest Products */}
                     <div className="col-lg-4 col-md-6">
-                        <ProductSlider title="Latest Products" slides={latestProductSlides} />
+                        <ProductSlider title="Latest Products" productGroups={latestProductSlides} />
                     </div>
-
-                    {/* Top Rated Products */}
                     <div className="col-lg-4 col-md-6">
-                        <ProductSlider title="Top Rated Products" slides={topRatedSlides} />
+                        <ProductSlider title="Top Rated Products" productGroups={topRatedSlides} />
                     </div>
-
-                    {/* Review Products */}
                     <div className="col-lg-4 col-md-6">
-                        <ProductSlider title="Review Products" slides={reviewSlides} />
+                        <ProductSlider title="Review Products" productGroups={reviewSlides} />
                     </div>
                 </div>
             </div>
